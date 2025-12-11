67093fab47d190b676dab6be6a424b4b23d9dac8
Ced59 <c.caudron59@gmail.com>
2025-12-11 22:40:48 +0100

    Compile et prerendering OK en angular 21!!

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,15 @@
+import '@angular/platform-server/init';
+
 import { provideServerRendering } from '@angular/ssr';
-import { ApplicationConfig, mergeApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import {
+  ApplicationConfig,
+  mergeApplicationConfig,
+  provideZoneChangeDetection
+} from '@angular/core';
+import {
+  bootstrapApplication,
+  BootstrapContext
+} from '@angular/platform-browser';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
@@ -14,6 +23,14 @@ const serverConfig: ApplicationConfig = {
 
 const config = mergeApplicationConfig(appConfig, serverConfig);
 
-export default function bootstrap() {
-  return bootstrapApplication(AppComponent, {...config, providers: [provideZoneChangeDetection(), ...config.providers]});
+// ⚠️ ICI : on reçoit le BootstrapContext et on le passe au bootstrapApplication
+export default function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(
+    AppComponent,
+    {
+      ...config,
+      providers: [provideZoneChangeDetection(), ...config.providers]
+    },
+    context
+  );
 }
