73aea8b16681c27603feb4a8372a512b934c7781
Ced59 <c.caudron59@gmail.com>
2025-12-11 22:20:41 +0100

    Migration angular 21 ok

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,5 +1,5 @@
 import { provideServerRendering } from '@angular/ssr';
-import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+import { ApplicationConfig, mergeApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
@@ -15,5 +15,5 @@ const serverConfig: ApplicationConfig = {
 const config = mergeApplicationConfig(appConfig, serverConfig);
 
 export default function bootstrap() {
-  return bootstrapApplication(AppComponent, config);
+  return bootstrapApplication(AppComponent, {...config, providers: [provideZoneChangeDetection(), ...config.providers]}, context);
 }
