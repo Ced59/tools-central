0243f9e91d48640b3163ae26c7baa8a66e274869
Ced59 <c.caudron59@gmail.com>
2026-01-02 10:43:29 +0100

    Ajout traductions et scrolling retour en haut à chaque page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,7 +9,13 @@ import {provideMatomo, withRouter} from "ngx-matomo-client";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',  // 👈 remet en haut à chaque navigation
+        anchorScrolling: 'enabled',        // 👈 supporte /page#section
+      }),
+    ),
     provideHttpClient(),
     provideClientHydration(),
     providePrimeNG({
